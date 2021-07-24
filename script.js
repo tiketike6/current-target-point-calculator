3aff616e20e878f85124564c1e521d015396ae76
tiketike6 <71871379+tiketike6@users.noreply.github.com>
2021-07-24 10:00:56 +0900

    更新ボタンの追加、読込・削除ボタンの無効切り替え、期間外の処理の変更

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
         const datetimeEnd = $('#datetime-end').val();
         const targetEnd = $('#target-end').val();
 
-        const regex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):[0-5][0-9]$/
+        const regex = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):[0-5][0-9]$/;
         if (!regex.test(datetimeStart) || !regex.test(datetimeEnd)) {
-            alert('日時は「2021-01-01T00:00」のように入力してください。')
+            alert('日時は「2021-01-01T00:00」のように入力してください。');
             return;
         }
         if (!dayjs(datetimeStart).isValid() || !dayjs(datetimeEnd).isValid() || !targetEnd) {
@@ -19,15 +19,21 @@
 
         const datetimeStartUnix = dayjs(datetimeStart).unix();
         const datetimeEndUnix = dayjs(datetimeEnd).unix();
-        const endOfTodayUnix = dayjs().endOf('d').unix();
-        const nowUnix = dayjs().unix();
+        let endOfTodayUnix = dayjs().endOf('d').unix();
+        if (endOfTodayUnix < datetimeStartUnix) {
+            endOfTodayUnix = dayjs.unix(datetimeStartUnix).endOf('d').unix();
+        }
+        if (endOfTodayUnix > datetimeEndUnix) {
+            endOfTodayUnix = datetimeEndUnix;
+        }
+        const nowUnix = dayjs().endOf('m').unix();
+        const targetToday = Math.round((targetEnd * (endOfTodayUnix - datetimeStartUnix)) / (datetimeEndUnix - datetimeStartUnix));
+        const targetNow = Math.round((targetEnd * (nowUnix - datetimeStartUnix)) / (datetimeEndUnix - datetimeStartUnix));
 
-        $('#label-today').text(`${dayjs().format('M/D')}の目標pt`);
-        $('#target-today').text(
-            Math.round((targetEnd * (endOfTodayUnix - datetimeStartUnix)) / (datetimeEndUnix - datetimeStartUnix)).toLocaleString()
-        );
-        $('#label-now').text(`${dayjs().format('M/D H:mm')}の目標pt`);
-        $('#target-now').text(Math.round((targetEnd * (nowUnix - datetimeStartUnix)) / (datetimeEndUnix - datetimeStartUnix)).toLocaleString());
+        $('#label-today').text(`${dayjs.unix(endOfTodayUnix).format('M/D')}の目標pt`);
+        $('#target-today').text(targetToday.toLocaleString());
+        $('#label-now').text(`${dayjs.unix(nowUnix).format('M/D H:mm')}の目標pt`);
+        $('#target-now').text(targetNow.toLocaleString());
 
         if ($('#auto-save').prop('checked')) {
             save();
@@ -40,7 +46,10 @@
     $('#target-end').change(calculateTargetPoint);
     $('#auto-save').change(calculateTargetPoint);
 
-    // 保存
+    // 更新ボタン
+    $('#update').click(calculateTargetPoint);
+
+    // 保存ボタン
     const save = () => {
         const datetimeSave = dayjs().format('YYYY/M/D H:mm');
 
@@ -55,10 +64,12 @@
         localStorage.setItem(location.href, JSON.stringify(saveData));
 
         $('#datetime-save').text(datetimeSave);
+        $('#load-save').prop('disabled', false);
+        $('#clear-save').prop('disabled', false);
     };
     $('#save').click(save);
 
-    // 入力の初期化
+    // 入力を初期化ボタン
     const defaultInput = () => {
         $('#datetime-start').val(dayjs().subtract(15, 'h').format('YYYY-MM-DDT15:00'));
         $('#datetime-end').val(dayjs().subtract(15, 'h').add(1, 'w').format('YYYY-MM-DDT20:59'));
@@ -69,7 +80,7 @@
     };
     $('#clear-input').click(defaultInput);
 
-    // 保存した値を読込
+    // 保存した値を読込ボタン
     const loadSavedData = () => {
         const savedString = localStorage.getItem(location.href);
 
@@ -87,16 +98,20 @@
         calculateTargetPoint();
 
         $('#datetime-save').text(savedData.datetimeSave);
+        $('#load-save').prop('disabled', false);
+        $('#clear-save').prop('disabled', false);
 
         return true;
     };
     $('#load-save').click(loadSavedData);
 
-    // 保存した値を削除
+    // 保存した値を削除ボタン
     $('#clear-save').click(() => {
         localStorage.removeItem(location.href);
 
         $('#datetime-save').text('削除済');
+        $('#load-save').prop('disabled', true);
+        $('#clear-save').prop('disabled', true);
     });
 
     // 画面表示時に保存した値を読込、保存した値がなければ入力の初期化
